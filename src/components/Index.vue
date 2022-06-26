220b1ebfc8a866eaa4bb4c4136c173b7ae327488
yulio64neos <julio64neos@gmail.com>
2022-06-26 00:20:36 -0500

    Se agregó un nuevo recurso vue-swiper

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,21 +1,9 @@
 <template>
   <div class="container">
     <section>
-      <!-- <div class="bg-light">
-         <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-             <div class="carousel-inner">
-                 <div class="carousel-item active">
-                     <img src="../assets/Caraturlas/yugi-kaiba.jpg" class="d-block w-100" alt="...">
-                 </div>
-                 <div class="carousel-item">
-                     <img src="../assets/Caraturlas/aniversario.jpg" class="d-block w-100" alt="...">
-                 </div>
-             </div>
-         </div>
-      </div> -->
       <div class="row">
-        <div class="col-lg-6">
-          <p class="titulo">Yu-Gi-Oh!</p>
+        <div class="col-lg-6 text-center">
+          <img class="logojap img-fluid" :src="imgBanner" />
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
             dictum eu purus sed eleifend. Nunc vitae justo lobortis, rutrum
@@ -24,14 +12,14 @@
           </p>
         </div>
         <div class="col-lg-6 text-center">
-          <img src="../assets/Caraturlas/dragonBlanco.png" class="img-fluid" />
+          <img :src="imgDragon" class="img-fluid" />
         </div>
       </div>
     </section>
     <section class="trio">
       <div class="row text-center">
         <div class="col-lg-4">
-          <img src="../assets/Caraturlas/cartas.png" class="img-fluid cartas" alt="" />
+          <img :src="imgCartas" class="img-fluid cartas" alt="" />
           <h4>Duelo de Cartas</h4>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
@@ -39,16 +27,16 @@
           </p>
         </div>
         <div class="col-lg-4">
-        <img src="../assets/Caraturlas/trio_yugi.png" class="img-fluid cartas" alt="" />
-        <h4>Yugi - Jaden - Yusei</h4>
+          <img :src="imgYugis" class="img-fluid cartas" alt="" />
+          <h4>Yugi - Jaden - Yusei</h4>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
             dictum eu purus sed eleifend.
           </p>
         </div>
         <div class="col-lg-4">
-          <img src="../assets/Caraturlas/Tea.png" class="img-fluid cartas" alt="" />
-        <h4>Tea</h4>
+          <img :src="imgTea" class="img-fluid cartas" alt="" />
+          <h4>Tea</h4>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
             dictum eu purus sed eleifend.
@@ -56,25 +44,147 @@
         </div>
       </div>
     </section>
+    <section class="trio">
+      <swiper
+        :slides-per-view="3"
+        :space-between="30"
+        :loop="false"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+      >
+        <swiper-slide
+          v-for="n in 5"
+          :key="n"
+          class="test"
+          :class="{ test_2: true }"
+        >
+          <div>{{ 29 + n }}</div>
+          <img
+            :src="getImageUrl(29 + n)"
+            width="600"
+            height="400"
+            class="img-fluid w-100 mx-auto"
+            blank="true"
+          />
+        </swiper-slide>
+      </swiper>
+      <swiper
+        :slides-per-view="3"
+        :space-between="30"
+        loop
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+      >
+        <swiper-slide
+          v-for="n in 5"
+          :key="n"
+          class="test"
+          :class="{ test_2: true }"
+        >
+          <div>{{ 34 + n }}</div>
+          <img
+            :src="getImageUrl(34 + n)"
+            width="600"
+            height="400"
+            class="img-fluid w-100 mx-auto"
+            blank="true"
+          />
+        </swiper-slide>
+      </swiper>
+
+      <h2>pagination - navigation</h2>
+      <swiper
+        :slides-per-view="3"
+        :space-between="30"
+        :loop="false"
+        :pagination="true"
+        :navigation="true"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+      >
+        <swiper-slide
+          v-for="n in 5"
+          :key="n"
+          class="test"
+          :class="{ test_2: true }"
+        >
+          <div>{{ 34 + n }}</div>
+          <img
+            :src="getImageUrl(34 + n)"
+            width="600"
+            height="400"
+            class="img-fluid w-100 mx-auto"
+            blank="true"
+          />
+        </swiper-slide>
+      </swiper>
+    </section>
   </div>
 </template>
 
 <script>
-export default {};
+import { Navigation, Pagination } from "swiper";
+import { SwiperCore, Swiper, SwiperSlide } from "swiper-vue2";
+import "swiper/swiper-bundle.css";
+SwiperCore.use([Navigation, Pagination]);
+export default {
+  data() {
+    return {
+      //Imagenes
+      imgBanner: require("../assets/Caraturlas/logoJapan.png"),
+      imgDragon: require("@/assets/Caraturlas/dragonBlanco.png"),
+      imgCartas: require("@/assets/Caraturlas/cartas.png"),
+      imgYugis: require("@/assets/Caraturlas/trio_yugi.png"),
+      imgTea: require("@/assets/Caraturlas/Tea.png"),
+    };
+  },
+
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  methods: {
+    getImageUrl(imageId) {
+      return `https://picsum.photos/600/400/?image=${imageId}`;
+    },
+    onSwiper(swiper) {
+      console.log(swiper);
+    },
+    onSlideChange() {
+      console.log("slide change");
+    },
+  },
+};
 </script>
 
-<style scoped>
-.titulo {
-  font-size: 80px;
-  font-weight: 600;
-  margin-top: 100px;
+<style lang="scss" scoped>
+.logojap {
+  width: 600px;
+  margin-top: 50px;
+  margin-bottom: 30px;
 }
 
 .trio {
   padding: 80px 0;
 }
-.cartas{
-    width: 100px;
-    margin-top: 20px;
+.cartas {
+  width: 100px;
+  margin-top: 20px;
+}
+
+.img-fluid {
+  max-width: 100%;
+  height: auto;
+}
+.w-100 {
+  width: 100%;
+}
+.ml-auto,
+.mx-auto {
+  margin-left: auto;
+}
+.mr-auto,
+.mx-auto {
+  margin-right: auto;
 }
 </style>
